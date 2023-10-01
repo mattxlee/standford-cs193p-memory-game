2b9ccc41953967a72d8f36e50a8f5d7a15c91fec
Matthew Lee <mattxlee@gmail.com>
2023-10-01 16:14:37 +0800

    Initial Commit

---
+++ MemoryCard/MemoryCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryCardApp.swift
+//  MemoryCard
+//
+//  Created by 李馨华 on 2023/10/1.
+//
+
+import SwiftUI
+
+@main
+struct MemoryCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
